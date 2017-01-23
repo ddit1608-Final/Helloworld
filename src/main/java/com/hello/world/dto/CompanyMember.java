c31eb6d38db75b0c75d1b1ff3b522d0d036c352d
Hong Hong <hoihoi207@naver.com>
2017-01-23 10:37:20 +0900

    companyVO >> Hong

---
+++ src/main/java/com/hello/world/dto/CompanyMember.java
@@ -0,0 +1,87 @@
+package com.hello.world.dto;
+
+public class CompanyMember {
+
+	private String compmem_bsnss_lcse_no; // 사업자 등록번호
+	private String compmem_mail; // 이메일
+	private String compmem_nm; // 가입자명
+	private String compmem_pw; // 패스워드
+	private String compmem_workp; // 근무지
+	private String compmem_ceo; // 대표자
+	private String compmem_comp_nm; // 회사명
+	private String compmem_phn; // 연락처
+
+	public String getCompmem_bsnss_lcse_no() {
+		return compmem_bsnss_lcse_no;
+	}
+
+	public void setCompmem_bsnss_lcse_no(String compmem_bsnss_lcse_no) {
+		this.compmem_bsnss_lcse_no = compmem_bsnss_lcse_no;
+	}
+
+	public String getCompmem_mail() {
+		return compmem_mail;
+	}
+
+	public void setCompmem_mail(String compmem_mail) {
+		this.compmem_mail = compmem_mail;
+	}
+
+	public String getCompmem_nm() {
+		return compmem_nm;
+	}
+
+	public void setCompmem_nm(String compmem_nm) {
+		this.compmem_nm = compmem_nm;
+	}
+
+	public String getCompmem_pw() {
+		return compmem_pw;
+	}
+
+	public void setCompmem_pw(String compmem_pw) {
+		this.compmem_pw = compmem_pw;
+	}
+
+	public String getCompmem_workp() {
+		return compmem_workp;
+	}
+
+	public void setCompmem_workp(String compmem_workp) {
+		this.compmem_workp = compmem_workp;
+	}
+
+	public String getCompmem_ceo() {
+		return compmem_ceo;
+	}
+
+	public void setCompmem_ceo(String compmem_ceo) {
+		this.compmem_ceo = compmem_ceo;
+	}
+
+	public String getCompmem_comp_nm() {
+		return compmem_comp_nm;
+	}
+
+	public void setCompmem_comp_nm(String compmem_comp_nm) {
+		this.compmem_comp_nm = compmem_comp_nm;
+	}
+
+	public String getCompmem_phn() {
+		return compmem_phn;
+	}
+
+	public void setCompmem_phn(String compmem_phn) {
+		this.compmem_phn = compmem_phn;
+	}
+
+	@Override
+	public String toString() {
+		return "CompanyMember [compmem_bsnss_lcse_no=" + compmem_bsnss_lcse_no
+				+ ", compmem_mail=" + compmem_mail + ", compmem_nm="
+				+ compmem_nm + ", compmem_pw=" + compmem_pw
+				+ ", compmem_workp=" + compmem_workp + ", compmem_ceo="
+				+ compmem_ceo + ", compmem_comp_nm=" + compmem_comp_nm
+				+ ", compmem_phn=" + compmem_phn + "]";
+	}
+}
