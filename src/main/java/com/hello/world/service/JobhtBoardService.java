2142c77868a026bd76d6bfb3a1e4891be8f466d3
Hong <Hong@222.234.71.83>
2017-01-23 21:41:50 +0900

    ..

---
+++ src/main/java/com/hello/world/service/JobhtBoardService.java
@@ -0,0 +1,13 @@
+package com.hello.world.service;
+
+import com.hello.world.dao.JobhtBoardDAO;
+
+public class JobhtBoardService {
+	
+	JobhtBoardDAO jobhtBoardDAO;
+	
+	public void setJobhtBoardDAO(JobhtBoardDAO jobhtBoardDAO){
+		this.jobhtBoardDAO= jobhtBoardDAO;
+	}
+
+}
