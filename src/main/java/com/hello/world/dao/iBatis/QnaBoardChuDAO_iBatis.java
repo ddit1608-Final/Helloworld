31ecf0b4bb1698cb815f14b5ba8397e8d2544709
Hong Hong <hoihoi207@naver.com>
2017-02-02 10:37:08 +0900

    asdf

--- src/main/java/com/hello/world/dao/iBatis/QnaBoardChuDAO_iBatis.java
+++ src/main/java/com/hello/world/dao/iBatis/QnaBoardChuDAO_iBatis.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.hello.world.dao.iBatis;
 
 import java.sql.SQLException;
@@ -26,35 +25,3 @@ public class QnaBoardChuDAO_iBatis implements QnaBoardChuDAO {
 	
 
 }
-=======
-package com.hello.world.dao.iBatis;
-
-import java.sql.SQLException;
-
-import com.hello.world.dao.QnaBoardChuDAO;
-import com.hello.world.dto.QnaBoardChuVO;
-import com.ibatis.sqlmap.client.SqlMapClient;
-
-public class QnaBoardChuDAO_iBatis implements QnaBoardChuDAO {
-	
-	private SqlMapClient client;
-
-	public void setClient(SqlMapClient client) {
-		this.client = client;
-	}
-
-	@Override
-	public int insertQnaBoardChu(QnaBoardChuVO qnaBoardChuVO)
-			throws SQLException {
-		
-		int result = -1;
-		
-		if(client.insert("insertQnaBoardChu", qnaBoardChuVO) == null)
-			
-		result = 1;
-		
-		return result;
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/ddit1608-Final/Helloworld.git
